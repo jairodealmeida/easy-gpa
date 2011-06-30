614d40acde2ca6fc6d27ab08c409b221a97fcfb3
jairodealmeida@gmail.com <jairodealmeida@gmail.com@d502f23e-f41f-f052-11c8-7e7022573b32>
2011-06-30 21:06:59 +0000

    alterações do entity

--- trunk/easy-gpa-test/src/br/com/slv/database/dao/entity/EntityTest.java
+++ trunk/easy-gpa-test/src/br/com/slv/database/dao/entity/EntityTest.java
@@ -3,32 +3,43 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import br.com.slv.database.dao.entity.Entity;
+import br.com.slv.database.dao.delegate.DataTransferDelegate;
+
 import br.com.slv.database.dao.model.TransferObject;
 
 
 
 public class EntityTest {
-	
+	DataTransferDelegate delegate = new DataTransferDelegate();
+
 	@Test
 	public void insert(){
-		Entity e = new Usuario("test","test","test", 1);
-		String result = e.insert();
+		Entity e = new Usuario(3,"test","test","test", 1);
+		String result = delegate.insert(e);
 		assertNotNull(result);
 		assertEquals("success", result);
 	}
+	
 	@Test
 	public void update(){
-		Entity e = new Usuario(6, "test", "test", "test", 1);
-		String result = e.update();
+		Entity e = new Usuario(3, "test", "test", "test", 1);
+		String result = delegate.update(e);
 		assertNotNull(result);
 		assertEquals("success", result);
 	}
+	
 	@Test
 	public void delete(){
 		Entity e = new Usuario(3, "test", "test", "test", 1);
-		String result = e.delete();
+		String result = delegate.delete(e);
 		assertNotNull(result);
 		assertEquals("success", result);
 	}
+	@Test
+	public void select(){
+		//TODO delegate.
+	}
+	
+
+	
 }
