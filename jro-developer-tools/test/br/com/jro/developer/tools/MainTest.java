ec4a47033e26543442531b2fb8e8211ea185884b
jairodealmeida <jairodealmeida@gmail.com>
2011-08-05 17:04:18 +0000

    merge command extension parameter

--- jro-developer-tools/test/br/com/jro/developer/tools/MainTest.java
+++ jro-developer-tools/test/br/com/jro/developer/tools/MainTest.java
@@ -40,6 +40,7 @@ public class MainTest {
         commands.add("-d C:\\ftp".split(" "));
         commands.add("-d null".split(" "));
         commands.add("C:\\ftp".split(" "));
+        commands.add("-m C:\\Temp txt".split(" "));
     }
 
     @AfterClass
